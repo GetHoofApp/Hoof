bae30459e05d8ea9376ab762f3aae91dd0e2cb5d
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-12-27 12:23:17 +0100

    Create SignUp module

---
+++ Hoof/Features/SignUp/SignUp/SignUp.h
@@ -0,0 +1,18 @@
+//
+//  SignUp.h
+//  SignUp
+//
+//  Created by Sameh Mabrouk on 27/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignUp.
+FOUNDATION_EXPORT double SignUpVersionNumber;
+
+//! Project version string for SignUp.
+FOUNDATION_EXPORT const unsigned char SignUpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignUp/PublicHeader.h>
+
+
